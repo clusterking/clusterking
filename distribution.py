b7bb090bd206dd1bf858f52b50ef8df234c02919
jasonaebischerGIT <jason.aebischer@tum.de>
2018-10-05 15:15:05 -0400

    distribution.py checked

--- distribution.py
+++ distribution.py
@@ -34,6 +34,7 @@ q2max = (mB -mD)**2
 
 
 
+
 def Elmax(q2):
 
     return np.sqrt(q2)/2.
@@ -43,15 +44,15 @@ Elmin =  0.
 
 
 
-
+#  A1
 
 def xval(q2):
-    
+
     return np.sqrt(q2/mtau**2)
 
 
 def yval(El):
-    
+
     return El/mtau
 
 
@@ -60,23 +61,23 @@ def yval(El):
 
 
 def I0w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap0val = Gammap0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam0val = Gammam0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam2val = Gammam2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammap2val =  Gammap2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     Ical1val = Ical1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
 
     return ( (3*x**2 -2)*(x+4*y)*(x-2*y)**2  )/( 6*x**2*(  x**2-1 )**2 *y**2    ) * Gammap0val   +  (  2*x**4 -3*x**2 -16*x*y**3 +12*y**2    )/(  6*x*(x**2-1)**2 *y**2 )*Gammam0val    +  (  20*x**5 *y  + x**4 *(40*y**2-6)    +16*x**3*y*(5*y**2-4)   +x**2*(15-72*y**2) -4*x*y*(8*y**2-5)   +20*y**2       )*(x-2*y)**2/(  120*x*(x**2-1)**4*y**4   )*Gammam2val   +  (   40*x**5*y   +5*x**4*(16*y**2-3)   -50*x**3*y  + x**2*(6-80*y**2)   +16*x*y +24*y**2   )*(x-2*y)**3/(   120*x**2*(x**2-1)**4*y**4 )*Gammap2val   +  ( -240*x**5*y**4  +9*x**5  +32*(10*x**4 -5*x**2 +1)*y**5  -30*(x**2+1)*x**4*y  +20*(x**4+4*x**2+1)*x**3*y**2       )/(120*x*(x**2-1   )**4*y**4)*Ical1val
 
@@ -88,13 +89,13 @@ def I0w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def I1w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap1val = Gammap1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam1val = Gammam1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
 
@@ -109,16 +110,16 @@ def I1w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def I2w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap2val = Gammap2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam2val = Gammam2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     Ical1val = Ical1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
     return  1/(120*(   x**2-1 )**4*y**4)*(  720*x**3*y**4  -64*(    5*(x**4+x**2)-1  )*y**5  -60*x**2 *(   x**4-2*x**2-2 )*y +9*x**3*(2*x**2-5)   +20*x*(    2*x**6 -x**4 -16*x**2 -3 )*y**2    )*Gammam2val  +  1/(120*x**2*(x**2-1)**4*y**4)*( -720*x**7*y**4 +9*(5*x**2-2)*x**5  -60*(   2*(x**4+x**2)-1    )*x**4*y     +64*(    5*(   3*x**6  -2*x**4 +x**2   )-1  )*y**5   +20*(    3*x**6  +16*x**4  +x**2 -2 )*x**3*y**2    )*Gammap2val  +  ( 240*x**5*y**4  -9*x**5  -32*(10*x**4 -5*x**2 +1)*y**5     +30*(x**2+1)*x**4*y  -20*(x**4 +4*x**2 +1)*x**3*y**2          )/(  40*x*(x**2-1)**4 *y**4  )*Ical1val
@@ -129,24 +130,24 @@ def I2w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def I0w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
-    
+
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap0val = Gammap0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam0val = Gammam0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammap2val = Gammap2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam2val = Gammam2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
-    Ical0val = Ical0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
+    #  Ical0val = Ical0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
+
+
     Ical1val = Ical1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
     return - 2*(2*x**2+1)*(4*x*y-3)/(3*x)*Gammam0val   +2*(x**2+2)*(3*x-4*y)/(3*x**2)*Gammap0val + 2/15*(     -12*x**2*y  +10*x  +5/x -8*y  )*Gammam2val   + (10*x*(x**2+2) - 8*(2*x**2+3)*y      )/(  15*x**2 )*Gammap2val    -4*(x**2-1)*y/(15*x)*Ical1val
@@ -156,18 +157,18 @@ def I0w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def I1w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap1val = Gammap1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam1val = Gammam1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     Ical0val = Ical0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     return   (   8*x**3*y  -4*x**2+2  )/(3*x)*Gammam1val  - 2*(x**3-2*x+4*y)/(3*x**2)*Gammap1val   +4/3*(-2*x*y -2*y/x +1)*Ical0val
 
 
@@ -176,19 +177,19 @@ def I1w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def I2w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     x = xval(q2)
-    
+
     y =yval(El)
-    
+
     Gammap2val = Gammap2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     Gammam2val = Gammam2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     Ical1val = Ical1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
-    
+
     return   8*(x**2-1)*y/(15*x**2)*Gammap2val  -8/15*( x**2-1 )*y*Gammam2val  +4*(x**2-1)*y/(5*x)*Ical1val
 
 
@@ -199,11 +200,11 @@ def I2w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 def I0(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
     if El >= mtau**2/(2 *np.sqrt(q2))  and El <= np.sqrt(q2)/2.:
-    
+
         return  I0w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     elif  El < mtau**2/(2 *np.sqrt(q2)) and El >= 0:
-        
+
         return  I0w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
 def I1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
@@ -211,20 +212,20 @@ def I1(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
     if El >= mtau**2/(2 *np.sqrt(q2))  and El <= np.sqrt(q2)/2.:
 
        return I1w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     elif  El < mtau**2/(2 *np.sqrt(q2)) and El >= 0:
-    
+
        return  I1w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
 
 def I2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
     if El >= mtau**2/(2 *np.sqrt(q2))  and El <= np.sqrt(q2)/2.:
-    
+
         return I2w1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     elif  El < mtau**2/(2 *np.sqrt(q2)) and El >= 0:
-        
+
         return  I2w2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
 
@@ -233,26 +234,26 @@ def I2(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 
 
 def dG(epsL, epsR, epsSR, epsSL, epsT, q2,  El , cthetal):
-    
+
     I0val =  I0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     I1val =  I1(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     I2val =  I2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
 
     return  Btaul * GF**2  * np.absolute(Vcb)**2 * new**2/(32 * np.pi**3) * kvec(q2)/mB**2 * (1-mtau**2/q2)**2 * El**2/mtau**3 *(  I0val     +  I1val * cthetal   + I2val * cthetal**2       )
 
 
-## integrating cthetal (result from alonso et al paper)
+## integrating cthetal (result from Alonso et al paper)
 
 def dGq2El(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
-    
+
     I0val =  I0(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
-    
+
+
     I2val =  I2(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     return  Btaul * GF**2  * np.absolute(Vcb)**2 * new**2/(16 * np.pi**3) * kvec(q2)/mB**2 * (1-mtau**2/q2)**2 * El**2/mtau**3 *(  I0val        + 1./3. * I2val       )
 
 
@@ -261,11 +262,11 @@ def dGq2El(epsL, epsR, epsSR, epsSL, epsT, q2,  El):
 ## 1D q2 distrubution, integrating El
 
 def dGq2(epsL, epsR, epsSR, epsSL, epsT, q2):
-    
+
     def tmp(El):
-        
+
         return dGq2El(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     return integrate.quad(lambda El: tmp(El),Elmin,Elmax(q2))[0]
 
 
@@ -283,31 +284,31 @@ def dGq2norm(epsL, epsR, epsSR, epsSL, epsT, q2):
 ##  Define lower limit of integration for q2, see Fig 4 in Alonso et al paper
 
 def q2inflim(El):
-    
+
     if El > mtau/2:
-        
+
         return   4*El**2
-        
+
     elif El <= mtau/2:
-            
+
         return q2min
 
 
 ## 1D El distrubution, integrating over q2
 
 def dGEl(epsL, epsR, epsSR, epsSL, epsT, El):
-    
+
     def tmp(q2):
-        
+
         return dGq2El(epsL, epsR, epsSR, epsSL, epsT, q2,  El)
-    
+
     return integrate.quad(lambda q2: tmp(q2), q2inflim(El),q2max)[0]
 
 
 ## normalized distribution 1D El
 
 def dGElnorm(epsL, epsR, epsSR, epsSL, epsT, El):
-    
+
     return tauBp/Btaul * dGEl(epsL, epsR, epsSR, epsSL, epsT, El)
 
 
@@ -315,7 +316,7 @@ def dGElnorm(epsL, epsR, epsSR, epsSL, epsT, El):
 
 
 def  dGq2cthetal(epsL, epsR, epsSR, epsSL, epsT, q2,cthetal):
-    
+
      def tmp(El):
          return dG(epsL, epsR, epsSR, epsSL, epsT, q2,  El , cthetal)
 
@@ -325,18 +326,18 @@ def  dGq2cthetal(epsL, epsR, epsSR, epsSL, epsT, q2,cthetal):
 
 
 def  dGcthetal(epsL, epsR, epsSR, epsSL, epsT,cthetal):
-    
+
      def tmp(q2):
-        
+
         return dGq2cthetal(epsL, epsR, epsSR, epsSL, epsT, q2,cthetal)
-            
+
      return integrate.quad(lambda q2: tmp(q2),q2min,q2max)[0]
 
 
 ## normalized distribution 1D cthetal
 
 def dGcthetalnorm(epsL, epsR, epsSR, epsSL, epsT, cthetal):
-    
+
     return tauBp/Btaul * dGcthetal(epsL, epsR, epsSR, epsSL, epsT,cthetal)
 
 
@@ -345,11 +346,11 @@ def dGcthetalnorm(epsL, epsR, epsSR, epsSL, epsT, cthetal):
 
 
 def dGtot(epsL, epsR, epsSR, epsSL, epsT):
-    
+
     def tmp(q2):
-        
+
         return dGq2(epsL, epsR, epsSR, epsSL, epsT, q2)
-    
+
     return integrate.quad(lambda q2: tmp(q2),q2min,q2max)[0]
 
 
@@ -363,7 +364,7 @@ def dGq2normtot(epsL, epsR, epsSR, epsSL, epsT,q2):
 ## El distribution normalized by total,  integral of this would be 1 by definition
 
 def dGElnormtot(epsL, epsR, epsSR, epsSL, epsT,El):
-    
+
     return dGEl(epsL, epsR, epsSR, epsSL, epsT, El)/dGtot(epsL, epsR, epsSR, epsSL, epsT)
 
 
@@ -371,21 +372,12 @@ def dGElnormtot(epsL, epsR, epsSR, epsSL, epsT,El):
 ## cthetal distribution normalized by total,  integral of this would be 1 by definition
 
 def dGcthetalnormtot(epsL, epsR, epsSR, epsSL, epsT,cthetal):
-    
+
     return dGcthetal(epsL, epsR, epsSR, epsSL, epsT, cthetal)/dGtot(epsL, epsR, epsSR, epsSL, epsT)
 
 
 ## full 3D distribution normalized by total,  3D integral of this would be 1 by definition
 
 def dGnormtot(epsL, epsR, epsSR, epsSL, epsT,q2,  El , cthetal):
-    
+
     return dG(epsL, epsR, epsSR, epsSL, epsT, q2,  El , cthetal)/dGtot(epsL, epsR, epsSR, epsSL, epsT)
-
-
-
-
-
-
-
-
-

ba4507ad2990b7a4e570701d5a705178d611643d
klieret <klieret@users.noreply.github.com>
2018-10-18 12:23:00 +0900

    measure speed and write out time estimate

--- scan.py
+++ scan.py
@@ -4,13 +4,8 @@
 
 from distribution import *
 
-import matplotlib.pyplot as plt
-import numpy
-
-
-from mpl_toolkits.mplot3d import Axes3D
-
-from matplotlib import cm
+import time
+import datetime
 
 
 ###
@@ -43,12 +38,28 @@ fresults = open(output, 'w')
 
 ##  I set epsR an epsSR to zero  as the observables are only sensitive to linear combinations  L + R
 
-for epsL in np.linspace(-0.30,0.30, np_grid_subdivisions):
-    for epsSL in np.linspace(-0.30,0.30, np_grid_subdivisions):
-        for epsT in np.linspace(-0.40,0.40, np_grid_subdivisions):
+performed_integrations = 0
+total_integrations = np_grid_subdivisions ** 4
+
+for epsL in np.linspace(-0.30, 0.30, np_grid_subdivisions):
+    for epsSL in np.linspace(-0.30, 0.30, np_grid_subdivisions):
+        for epsT in np.linspace(-0.40, 0.40, np_grid_subdivisions):
             for q2 in np.linspace(q2min,q2max, grid_subdivisions):
-                dist_tmp = dGq2normtot(epsL,0,0, epsSL, epsT,q2)
+                start = time.time()
+                dist_tmp = dGq2normtot(epsL, 0, 0, epsSL, epsT,q2)
+                elapsed_time = time.time() - start
+                remaining_time = (total_integrations - performed_integrations) * elapsed_time
                 fresults.write('%.5f    '%epsL  + '%.5f    '%epsSL  + '%.5f  '%epsT  + '%.5f   '%q2  + '%.10f       '%dist_tmp + '\n')
+                performed_integrations += 1
+                print("Progress: {}/{} ({:.2f}%). Last timing: {:.1f}s => "
+                      "time remaining: {}".format(
+                        performed_integrations,
+                        total_integrations,
+                        100*performed_integrations/total_integrations,
+                        elapsed_time,
+                        datetime.timedelta(seconds=remaining_time)
+                ))
+
 
 fresults.close()
 

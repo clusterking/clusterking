5dafcb66105f12141d9714fcdede623c73668c94
klieret <klieret@users.noreply.github.com>
2018-10-23 11:49:01 +0900

    CRITICAL FIX: accessing dictionaries

--- modules/form_factors.py
+++ modules/form_factors.py
@@ -1,50 +1,18 @@
 #!/usr/bin/env python3
 
-import numpy as np
-
-import flavio
-from math import sqrt,radians,asin
-from flavio.physics.bdecays.formfactors.b_p import btop, bcl_parameters, bcl
-import copy
+from flavio.physics.bdecays.formfactors.b_p import bcl
 from flavio.parameters import default_parameters
-from flavio.classes import Parameter, Implementation
 
-
-
-c = copy.deepcopy(default_parameters)
-par = c.get_central_all()
-
-
-
-
-#  {'f+','f0','fT'}
+default_central_values = default_parameters.get_central_all()
 
 
 def fplus(q2):
+    return bcl.ff_isgurwise('B->D', q2, default_central_values, 4.8, n=3)['f+']
     
-    fflist = list(bcl.ff_isgurwise('B->D',q2, par, 4.8, n=3).values())
-    
-    return  fflist[0]
-
-
 
 def fzero(q2):
-    
-    fflist = list(bcl.ff_isgurwise('B->D',q2, par, 4.8, n=3).values())
-    
-    return  fflist[1]
-
-
+    return bcl.ff_isgurwise('B->D', q2, default_central_values, 4.8, n=3)['f0']
 
 
 def fT(q2):
-    
-    fflist = list(bcl.ff_isgurwise('B->D',q2, par, 4.8, n=3).values())
-    
-    return  fflist[2]
-
-
-
-#print( fplus(4),fzero(4),fT(4)  )
-
-#print(bcl.ff_isgurwise('B->D', 4, par, 4.8, n=3))
+    return bcl.ff_isgurwise('B->D', q2, default_central_values, 4.8, n=3)['fT']

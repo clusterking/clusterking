affe861f2b6748ab6efbfcccdf40627141e5e999
klieret <klieret@users.noreply.github.com>
2018-10-30 23:15:47 +0900

    more flexibility in setting up NP grid

--- scan.py
+++ scan.py
@@ -55,6 +55,11 @@ def calculate_bpoint(w: Wilson, bin_edges: np.array) -> np.array:
 
 class Scanner(object):
     """ This implements all the functionality. """
+
+    # **************************************************************************
+    # A:  Setup
+    # **************************************************************************
+
     def __init__(self):
         self.log = get_logger("Scanner")
 
@@ -76,6 +81,7 @@ class Scanner(object):
         """ Manually set the edges of the q2 binning. """
         self._q2points = q2points
         self.config["q2points"]["sampling"] = "manual"
+        self.config["q2points"]["nbins"] = len(self._q2points)
 
     def set_q2points_equidist(
         self,
@@ -95,96 +101,66 @@ class Scanner(object):
         """
         self._q2points = np.linspace(dist_min, dist_max, no_bins+1)
         self.config["q2points"]["sampling"] = "equidistant"
+        self.config["q2points"]["nbins"] = len(self._q2points)
+        self.config["q2points"]["min"] = dist_min
+        self.config["q2points"]["max"] = dist_max
 
     def set_bpoints_manual(self, bpoints) -> None:
         """ Manually set a list of benchmark points """
         self._bpoints = bpoints
         self.config["bpoints"]["sampling"] = "manual"
+        self.config["bpoints"]["npoints"] = len(self._bpoints)
 
-    # todo: make method more flexible
-    def set_bpoints_equidist(self, sampling=20) -> None:
+    def set_bpoints_equidist(self, sampling=20, minima=None, maxima=None) -> None:
         """ Set a list of 'equidistant' benchmark points. """
-        bpoints = []
+
         # I set epsR an epsSR to zero  as the observables are only sensitive to
         # linear combinations  L + R
-        eps_r = 0.
-        eps_sr = 0.
-        for eps_l in np.linspace(-0.30, 0.30, sampling):
-            for eps_sl in np.linspace(-0.30, 0.30, sampling):
-                for eps_t in np.linspace(-0.40, 0.40, sampling):
-                    bpoints.append(Wilson(eps_l, eps_r, eps_sr, eps_sl, eps_t))
+
+        _min = minima
+        if not _min:
+            _min = {
+                'l': -0.3,
+                'r': 0.,
+                'sl': -0.3,
+                'sr': 0.,
+                't': -0.4
+            }
+
+        _max = maxima
+        if not _max:
+            _max = {
+                'l': 0.3,
+                'r': 0.,
+                'sl': 0.3,
+                'sr': 0.,
+                't': 0.4
+            }
+
+        if isinstance(sampling, int):
+            _sam = {key: sampling for key in ['l', 'r', 'sl', 'sr', 't']}
+        elif isinstance(sampling, dict):
+            _sam = sampling
+        else:
+            raise ValueError("Can't handle that type.")
+
+        bpoints = []
+        for l in np.linspace(_min['l'], _max['l'], _sam['l']):
+            for r in np.linspace(_min['r'], _max['r'], _sam['r']):
+                for sr in np.linspace(_min['sr'], _max['sr'], _sam['sr']):
+                    for sl in np.linspace(_min['sl'], _max['sl'], _sam['sl']):
+                        for t in np.linspace(_min['t'], _max['t'], _sam['t']):
+                            bpoints.append(Wilson(l, r, sr, sl, t))
         self._bpoints = bpoints
         self.config["bpoints"]["sampling"] = "equidistant"
+        self.config["bpoints"]["npoints"] = len(self._bpoints)
+        self.config["bpoints"]["min"] = _min
+        self.config["bpoints"]["max"] = _max
+        self.config["bpoints"]["sample"] = _sam
 
-    # todo: docstring
-    @staticmethod
-    def data_output_path(general_output_path):
-        return os.path.join(
-            os.path.dirname(general_output_path),
-            os.path.basename(general_output_path) + "_data.csv"
-        )
-
-    # todo: docstring
-    @staticmethod
-    def config_output_path(general_output_path):
-        return os.path.join(
-            os.path.dirname(general_output_path),
-            os.path.basename(general_output_path) + "_config.json"
-        )
-
-    def write(self, general_output_path) -> None:
-        """ Write out all results.
-        IMPORTANT NOTE: All output files will always be overwritten!
-
-        Args:
-            general_output_path: Path to the output file without file extension.
-            We will add suffixes and file extensions to this!
-        """
-        if self.df.empty:
-            self.log.error("Data frame is empty yet attempting to write out. "
-                           "Ignore.")
-            return
-
-        # *** 1. Clean files and make sure the folders exist ***
-
-        config_path = self.config_output_path(general_output_path)
-        data_path = self.data_output_path(general_output_path)
-
-        self.log.info("Will write config to '{}'.".format(config_path))
-        self.log.info("Will write data to '{}'.".format(data_path))
-
-        paths = [config_path, data_path]
-        for path in paths:
-            dirname = os.path.dirname(path)
-            if dirname and not os.path.exists(dirname):
-                self.log.debug("Creating directory '{}'.".format(dirname))
-                os.makedirs(dirname)
-            if os.path.exists(path):
-                self.log.debug("Removing file '{}'.".format(path))
-                os.remove(path)
-
-        # *** 2. Write out config ***
-
-        self.log.debug("Converting config data to json and writing to file "
-                       "'{}'.".format(config_path))
-        global_config = {"scan": self.config}
-        with open(config_path, "w") as config_file:
-            json.dump(global_config, config_file, sort_keys=True, indent=4)
-        self.log.debug("Done.")
-
-        # *** 3. Write out data ***
-
-        self.log.debug("Converting data to csv and writing to "
-                       "file '{}'.".format(data_path))
-        if self.df.empty:
-            self.log.error("Dataframe seems to be empty. Still writing "
-                           "out anyway.")
-        with open(data_path, "w") as data_file:
-            self.df.to_csv(data_file)
-        self.log.debug("Done.")
-
-        self.log.info("Writing out finished.")
-
+    # **************************************************************************
+    # B:  Run
+    # **************************************************************************
 
     def run(self, no_workers=4) -> None:
         """Calculate all benchmark points in parallel and saves the result in
@@ -243,11 +219,109 @@ class Scanner(object):
         pool.join()
 
         self.log.debug("Converting data to pandas dataframe.")
-        cols = ["eps_l", "eps_r", "eps_sr", "eps_sl", "eps_t"]
+        cols = list(Wilson(0, 0, 0, 0, 0).dict().keys())
         cols.extend(["bin{}".format(no_bin) for no_bin in range(len(self._q2points)-1)])
         self.df = pd.DataFrame(data=rows, columns=cols)
 
         self.log.info("Integration done.")
+
+    # **************************************************************************
+    # C:  Write out
+    # **************************************************************************
+
+    @staticmethod
+    def data_output_path(general_output_path):
+        """ Taking the general output path, return the path to the data file.
+        """
+        return os.path.join(
+            os.path.dirname(general_output_path),
+            os.path.basename(general_output_path) + "_data.csv"
+        )
+
+    @staticmethod
+    def config_output_path(general_output_path):
+        """ Taking the general output path, return the path to the config file.
+        """
+        return os.path.join(
+            os.path.dirname(general_output_path),
+            os.path.basename(general_output_path) + "_config.json"
+        )
+
+    @staticmethod
+    def index2wilson_output_path(general_output_path):
+        """ Taking the general output path, return the path to the config file.
+        """
+        return os.path.join(
+            os.path.dirname(general_output_path),
+            os.path.basename(general_output_path) + "_index2wilson.csv"
+        )
+
+    def write(self, general_output_path) -> None:
+        """ Write out all results.
+        IMPORTANT NOTE: All output files will always be overwritten!
+
+        Args:
+            general_output_path: Path to the output file without file extension.
+            We will add suffixes and file extensions to this!
+        """
+        if self.df.empty:
+            self.log.error("Data frame is empty yet attempting to write out. "
+                           "Ignore.")
+            return
+
+        # *** 1. Clean files and make sure the folders exist ***
+
+        config_path = self.config_output_path(general_output_path)
+        data_path = self.data_output_path(general_output_path)
+        index2wilson_path = self.index2wilson_output_path(general_output_path)
+
+        self.log.info("Will write config to '{}'.".format(config_path))
+        self.log.info("Will write data to '{}'.".format(data_path))
+        self.log.info("Will write index -> wilson mapping to '{}'.".format(
+            index2wilson_path))
+
+        paths = [config_path, data_path, index2wilson_path]
+        for path in paths:
+            dirname = os.path.dirname(path)
+            if dirname and not os.path.exists(dirname):
+                self.log.debug("Creating directory '{}'.".format(dirname))
+                os.makedirs(dirname)
+            if os.path.exists(path):
+                self.log.debug("Removing file '{}'.".format(path))
+                os.remove(path)
+
+        # *** 2. Write out config ***
+
+        self.log.debug("Converting config data to json and writing to file "
+                       "'{}'.".format(config_path))
+        global_config = {"scan": self.config}
+        with open(config_path, "w") as config_file:
+            json.dump(global_config, config_file, sort_keys=True, indent=4)
+        self.log.debug("Done.")
+
+        # *** 3. Write out data ***
+
+        self.log.debug("Converting data to csv and writing to "
+                       "file '{}'.".format(data_path))
+        if self.df.empty:
+            self.log.error("Dataframe seems to be empty. Still writing "
+                           "out anyway.")
+        wilson_cols = list(Wilson(0, 0, 0, 0, 0).dict().keys())
+        with open(data_path, "w") as data_file:
+            self.df.drop(columns=wilson_cols).to_csv(data_file)
+        self.log.debug("Done")
+
+        # *** 4. Write out index -> Wilson mapping ***
+
+        self.log.debug("Converting index2wilson to csv and writing to "
+                       "file '{}'.".format(index2wilson_path))
+        with open(index2wilson_path, "w") as index2wilson_file:
+            self.df[wilson_cols].to_csv(index2wilson_file)
+        self.log.debug("Done")
+
+        # *** 5. Done ***
+
+        self.log.info("Writing out finished.")
 
 
 def cli():
@@ -291,7 +365,8 @@ def cli():
         args.q2_bins))
 
     paths = [s.config_output_path(args.output_path),
-             s.data_output_path(args.output_path)]
+             s.data_output_path(args.output_path),
+             s.index2wilson_output_path(args.output_path)]
     existing_paths = [ path for path in paths if os.path.exists(path) ]
     if existing_paths:
         agree = yn_prompt("Output paths {} already exist(s) and will be "

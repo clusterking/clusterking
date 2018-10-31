23961d4129ed79497e8b3f5cc8c6c606b68ddd7e
klieret <klieret@users.noreply.github.com>
2018-10-31 12:30:48 +0900

    Dendogram is now configurable

--- cluster.py
+++ cluster.py
@@ -8,6 +8,7 @@ import argparse
 import json
 import os.path
 import sys
+from typing import Union
 
 # 3rd party
 import pandas as pd
@@ -50,32 +51,76 @@ class Cluster(object):
         self.log.debug("Done.")
 
     def build_hierarchy(self, metric="euclidean", method="complete"):
-        # values =
-        # matrix =
         nbins = self.scan_metadata["scan"]["q2points"]["nbins"]
         # only the q2 bins without any other information in the dataframe
         data = self.scan_df[["bin{}".format(i) for i in range(nbins)]]
         self.hierarchy = linkage(data, metric=metric, method=method)
 
-    def dendogram(self, output_path=None):
+    def dendogram(
+            self,
+            output: Union[None, str]=None,
+            ax=None,
+            show=False,
+            **kwargs
+    ) -> plt.Axes:
+        """Creates dendogram
+
+        Args:
+            output: If supplied, we save the dendogram there
+            ax: An axes object if you want to add the dendogram to an existing
+                axes rather than creating a new one
+            show: If true, the dendogram is shown in a viewer.
+
+        Returns:
+            The matplotlib.pyplot.Axes object
+        """
         if self.hierarchy is None:
             self.log.error("Hierarchy not yet set up. Returning without "
                            "doing anything.")
             return
-        labelsize=20
-        ticksize=15
-        plt.title('Hierarchical Clustering Dendrogram', fontsize=labelsize)
-        plt.xlabel('ID', fontsize=labelsize)
-        plt.ylabel('distance', fontsize=labelsize)
+
+        # do we add to a plot or generate a whole new figure?
+        if ax:
+            fig = ax.get_figure()
+        else:
+            fig, ax = plt.subplots()
+
+        labelsize = 20
+        ticksize = 15
+        ax.set_title('Hierarchical Clustering Dendrogram', fontsize=labelsize)
+        ax.set_xlabel('ID', fontsize=labelsize)
+        ax.set_ylabel('Distance', fontsize=labelsize)
+
+        # set defaults for dendogram plotting options here
+        # (this way we can overwrite them with additional arguments)
+        den_config = {
+            "color_threshold": "default",
+            "leaf_rotation": 90.,  # rotates the x axis labels
+            "leaf_font_size": 8,   # font size for the x axis labels
+        }
+        den_config.update(kwargs)
+
         den = dendrogram(
             self.hierarchy,
-            color_threshold = 0.02,
-            leaf_rotation=90.,  # rotates the x axis labels
-            leaf_font_size=8,   # font size for the x axis labels
+            ax=ax,
+            **den_config
         )
-        if not output_path:
-            plt.show()
 
+        if show:
+            # Note: we do not block here, so make sure that you're including
+            # plt.show() or an input statement somewhere in this script so
+            # that it doesn't just exit.
+            fig.show()
+        if output:
+            assert(isinstance(output, str))
+            dirname = os.path.dirname(output)
+            if dirname and not os.path.exists(dirname):
+                self.log.debug("Creating dir '{}'.".format(dirname))
+                os.makedirs(dirname)
+            fig.savefig(output, bbox_inches="tight")
+            self.log.info("Wrote dendogram to '{}'.".format(output))
+
+        return ax
 
 
 def cli():
@@ -86,12 +131,12 @@ def cli():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input",
-                        help="Input file/distance matrix.",
+                        help="Input file basename",
                         default="output/scan/global_results",
                         dest="input_path")
     parser.add_argument("-o", "--output",
-                        help="Output file.",
-                        default="output/cluster/global_results.out",
+                        help="Output file basename",
+                        default="output/cluster/global_results",
                         dest="output_path")
     args = parser.parse_args()
 
@@ -107,11 +152,9 @@ def cli():
 
     c.log.info("Output file: '{}'".format(args.output_path))
 
-    print(c)
-
     c.build_hierarchy()
-    c.dendogram()
-    input()
+    c.dendogram(show=True, output="test.pdf")
+    plt.show()
 
 if __name__ == "__main__":
     # Run command line interface

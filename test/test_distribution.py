766b5d7febf9065b24fa571d88001433d6b760ce
klieret <klieret@users.noreply.github.com>
2018-11-25 13:08:32 +0100

    fix test_distribution unit test

--- test/test_distribution.py
+++ test/test_distribution.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 
 import unittest
-
+import numpy as np
 from modules.distribution import *
 
 
 class TestDistribution(unittest.TestCase):
     def test_bin_function(self):
-        self.assertEqual(bin_function(lambda x: 1, [1, 2, 3]),
-                         [1, 1])
-        self.assertEqual(bin_function(lambda x: 1, [1, 2, 3], normalized=True),
-                         [0.5, 0.5])
-        self.assertEqual(bin_function(lambda x: x, [0, 1]),
-                         [0.5])
-        self.assertEqual(bin_function(lambda x: x, [0, 1], midpoints=True),
-                         [(0.5, 0.5)])
-        self.assertEqual(bin_function(lambda x: x, [0, 1], midpoints=True,
-                                      normalized=True),
-                         [(0.5, 1)])
-        # bin_function(lambda x: x**2, [1, ])
+        self.assertSequenceEqual(
+            list(bin_function(lambda x: 1, np.array([1, 2, 3]))),
+            [1, 1])
+        self.assertSequenceEqual(
+            list(bin_function(lambda x: 1, np.array([1, 2, 3]), normalized=True)),
+            [0.5, 0.5])
+        self.assertSequenceEqual(
+            list(bin_function(lambda x: x, np.array([0, 1]))),
+            [0.5])

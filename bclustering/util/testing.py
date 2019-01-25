c6cf9a90e1954a897d284f32e7d9f870a756521d
klieret <klieret@users.noreply.github.com>
2019-01-25 16:02:16 +0100

    unittest running parallel

--- bclustering/util/testing.py
+++ bclustering/util/testing.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
+# std
 import os
+import pathlib
+
+# 3rd party
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
 
 ENV_VAR_TESTING_MODE = "B_decays_clustering_TESTMODE"
 
@@ -30,3 +36,21 @@ def is_testing_mode():
     else:
         raise ValueError("{} set to invalid value {}.".format(
             ENV_VAR_TESTING_MODE, testing_mode))
+
+
+def test_jupyter_notebook(path) -> None:
+    """ Runs jupyter notebook. A ValueError is raised if the file was 
+    not found. """
+    set_testing_mode(True)
+    path = pathlib.Path(path)
+    if not path.exists():
+        raise ValueError("Notebook '{}' wasn't even found!".format(path))
+    run_path_str = str(path.parent.resolve())
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    try:
+        with path.open() as f:
+            nb = nbformat.read(f, as_version=4)
+            ep.preprocess(nb, {'metadata': {'path': run_path_str}})
+    except Exception as e:
+        set_testing_mode(False)
+        raise e

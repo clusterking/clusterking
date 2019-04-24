c9446ea0c5e3357d0d171f1ceb5e7497c582307a
klieret <klieret@users.noreply.github.com>
2019-04-24 16:43:11 +0200

    [Scan] WIP Feature: Add complex values to wilson coefficients

--- clusterking/scan/test/test_wilsonscanner.py
+++ clusterking/scan/test/test_wilsonscanner.py
@@ -13,7 +13,7 @@ def test_func(w, q):
     return q+1
 
 
-class TestWilsonScanner(MyTestCase):
+class TestWilsonScannerRun(MyTestCase):
     def setUp(self):
         self.s = WilsonScanner()
         self.s.set_spoints_equidist(
@@ -38,6 +38,40 @@ class TestWilsonScanner(MyTestCase):
         self.assertEqual(self.d.n, 8)
         self.assertEqual(self.d.nbins, 2)
         self.assertEqual(self.d.npars, 3)
+
+
+class TestWilsonScanner(MyTestCase):
+
+    def test_spoints_equidist(self):
+        s = WilsonScanner()
+        s.set_spoints_equidist(
+            {
+                "CVL_bctaunutau": (-1, 1, 2),
+                "CSL_bctaunutau": (-1, 1, 3),
+                "CT_bctaunutau": (-1, 1, 4)
+            },
+            scale=5,
+            eft='WET',
+            basis='flavio'
+        )
+        self.assertEqual(
+            len(s.spoints), 2*3*4
+        )
+
+    def test_spoints_equidist_complex(self):
+        s = WilsonScanner()
+        s.set_spoints_equidist(
+            {
+                "CVL_bctaunutau": (-1, 1, 2),
+                "im_CVL_bctaunutau": (-1, 1, 2),
+            },
+            scale=5,
+            eft='WET',
+            basis='flavio'
+        )
+        self.assertEqual(
+            len(s.spoints), 2*2
+        )
 
 
 if __name__ == "__main__":
--- clusterking/scan/wilsonscanner.py
+++ clusterking/scan/wilsonscanner.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+# std
+from typing import Dict
+import itertools
+
 # 3rd
 import wilson
 import numpy as np
-import itertools
 
 # ours
 from clusterking.scan.scanner import Scanner
@@ -54,6 +57,18 @@ class WilsonScanner(Scanner):
     def __init__(self):
         """ Initializes the :class:`clusterking.scan.WilsonScanner` class. """
         super().__init__()
+        self.imaginary_prefix = "im_"
+
+    @property
+    def imaginary_prefix(self) -> str:
+        """ Prefix for the name of imaginary parts of coefficients.
+        Also see the documentation of :meth:`.set_spoints_equidistant`.
+        """
+        return self.md["imaginary_prefix"]
+
+    @imaginary_prefix.setter
+    def imaginary_prefix(self, value: str) -> None:
+        self.md["imaginary_prefix"] = value
 
     def set_spoints_grid(self, values, scale, eft, basis) -> None:
         """ Set a grid of points in wilson space.
@@ -65,11 +80,14 @@ class WilsonScanner(Scanner):
 
                     {
                         <wilson coeff name>: [
-                            value1,
-                            value2,
-                            ...
+                            value_1,
+                            ...,
+                            value_n
                         ]
                     }
+
+                where ``value_1``, ..., ``value_n`` can be complex numbers in
+                general.
 
             scale: Wilson coeff input scale in GeV
             eft: Wilson coeff input eft
@@ -113,7 +131,8 @@ class WilsonScanner(Scanner):
         md["eft"] = eft
         md["basis"] = basis
 
-    def set_spoints_equidist(self, ranges, scale, eft, basis) -> None:
+    def set_spoints_equidist(self, ranges: Dict[str, tuple], scale: float,
+                             eft: str, basis: str) -> None:
         """ Set a list of 'equidistant' points in wilson space.
 
         Args:
@@ -133,14 +152,69 @@ class WilsonScanner(Scanner):
             eft: <Wilson coeff input eft>,
             basis: <Wilson coeff input basis>
 
+        .. note::
+
+            In order to add imaginary parts to your wilson coefficients,
+            prepend their name with ``im_`` (you can customize this prefix by
+            setting the :attr:`.imaginary_prefix` attribute to a custom value.)
+
+            Example:
+
+            .. code-block:: python
+
+                ws = WilsonScanner()
+                ws.set_spoints_equidist(
+                    {
+                        "a": (-2, 2, 4),
+                        "img_a": (-1, 1, 10),
+                    },
+                    ...
+                )
+
+            Will sample the real part of ``a`` in 4 points between -2 and 2 and
+            the imaginary part of ``a`` in 10 points between -1 and 1.
+
         Returns:
             None
         """
+        # Because of our hack with the imaginary prefix, let's first see which
+        # coefficients we really have
 
-        grid_config = {
-            coeff: list(np.linspace(*ranges[coeff]))
-            for coeff in ranges
-        }
+        def is_imaginary(name: str) -> bool:
+            return name.startswith(self.imaginary_prefix)
+
+        def real_part(name: str) -> str:
+            if is_imaginary(name):
+                return name.replace(self.imaginary_prefix, "", 1)
+            else:
+                return name
+
+        def imaginary_part(name: str) -> str:
+            if not is_imaginary(name):
+                return self.imaginary_prefix + name
+            else:
+                return name
+
+        coeffs = list(set(map(real_part, ranges.keys())))
+
+        grid_config = {}
+        for coeff in coeffs:
+            # Now let's always collect the values of the real part and of the
+            # imaginary part
+            res = [0.]
+            ims = [0.]
+            if real_part(coeff) in ranges:
+                res = list(np.linspace(*ranges[real_part(coeff)]))
+            if imaginary_part(coeff) in ranges:
+                ims = list(np.linspace(*ranges[imaginary_part(coeff)]))
+            # And basically take their cartesian product, alias initialize
+            # the complex number.
+            grid_config[coeff] = [
+                complex(x, y)
+                for x in res
+                for y in ims
+            ]
+
         self.set_spoints_grid(
             grid_config,
             scale=scale,

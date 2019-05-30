a3ccba6cd71c87f9c8bca29ea75d4ac324a0de88
Kilian Lieret <kilian.adriano.lieret@desy.de>
2019-05-30 18:20:53 +0200

    [Dev] Remove dirs created during build process

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,5 +5,6 @@ set -e
 thisDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sourceDir="${thisDir}/.."
 
-rm -rf "${sourceDir}/*.egg-info"
-rm -rf "${sourceDir}/build/**"
+rm -rf "${sourceDir}/clusterking.egg-info"
+rm -rf "${sourceDir}/build"
+rm -rf "${sourceDir}/dist"

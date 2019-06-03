835f4dec5596ee0d60cd2b613effdddd0240be98
klieret <klieret@users.noreply.github.com>
2019-06-03 18:03:07 +0200

    [Doc] Fix: Docstring in wrong __init__ file

--- clusterking/__init__.py
+++ clusterking/__init__.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
 
-# todo: why is this docstring not recognized in sphinx?
-"""
-This module bundles mostly technical utilities that might not be all this
-interesting for users.
-"""
-
 import clusterking.cluster
 import clusterking.data
 import clusterking.maths

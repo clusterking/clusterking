2b8c3e4b9e9177f12a80a60958f6ae5a00b5b97b
klieret <klieret@users.noreply.github.com>
2019-06-04 17:15:23 +0200

    [Enh] Stabilitytester result object is basically df

--- clusterking/stability/subsamplestability.py
+++ clusterking/stability/subsamplestability.py
@@ -2,6 +2,7 @@
 
 # 3rd
 import tqdm
+import pandas as pd
 
 # ours
 from clusterking.stability.clustermatcher import TrivialClusterMatcher
@@ -9,21 +10,8 @@ from clusterking.util.matplotlib_utils import import_matplotlib
 
 
 class SubSampleStabilityTesterResult(object):
-    def __init__(self, foms, nclusters):
-        self.foms = foms
-        self.nclusters = nclusters
-
-    def hist_foms(self):
-        import_matplotlib()
-        import matplotlib.pyplot as plt
-
-        plt.hist(self.foms, density=True)
-
-    def hist_nclusters(self):
-        import_matplotlib()
-        import matplotlib.pyplot as plt
-
-        plt.hist(self.nclusters, density=True)
+    def __init__(self, df):
+        self.df = df
 
 
 def default_fom(clustered1, clustered2):
@@ -40,6 +28,8 @@ class SubSampleStabilityTester(object):
         self.set_cluster_matcher()
         self._fom = None
         self.set_fom()
+        self._progress_bar = None
+        self.set_progress_bar()
 
     def set_basic_config(self, fraction=0.75, repeat=100):
         assert 0 < fraction < 1
@@ -52,12 +42,20 @@ class SubSampleStabilityTester(object):
     def set_fom(self, fct=default_fom):
         self._fom = fct
 
+    def set_progress_bar(self, state=True):
+        self._progress_bar = state
+
     def run(self, data, worker):
         foms = []
         nclusters = []
+        match_losts = []
         default_clusters = worker.run(data).get_clusters(indexed=True)
         matcher = self._cluster_matcher()
-        for i in tqdm.tqdm(range(self._repeat)):
+        if self._progress_bar:
+            iterator = tqdm.tqdm(range(self._repeat))
+        else:
+            iterator = range(self._repeat)
+        for i in iterator:
             r = worker.run(data.sample_param_random(frac=self._fraction))
             clusters = r.get_clusters(indexed=True)
             relevant_default_clusters = default_clusters[clusters.index]
@@ -67,4 +65,10 @@ class SubSampleStabilityTester(object):
             foms.append(fom)
             ncluster = len(set(clusters))
             nclusters.append(ncluster)
-        return SubSampleStabilityTesterResult(foms=foms, nclusters=nclusters)
+            match_lost = len(rename_dct.keys()) - len(set(rename_dct.values()))
+            match_losts.append(match_lost)
+        df = pd.DataFrame()
+        df["fom"] = foms
+        df["nclusters"] = nclusters
+        df["match_lost"] = match_losts
+        return SubSampleStabilityTesterResult(df=df)

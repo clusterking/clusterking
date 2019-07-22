2e0f17d8a6b2185ab34ca9727708d68ce24797a6
klieret <klieret@users.noreply.github.com>
2019-07-22 14:06:09 +0200

    [Feat, Ref] Saving noisy sample dataframes for speedup

--- clusterking/stability/noisysamplestability.py
+++ clusterking/stability/noisysamplestability.py
@@ -3,7 +3,8 @@
 # std
 import copy
 import collections
-from typing import Optional
+from typing import Optional, Union, List
+from pathlib import PurePath, Path
 
 # 3rd
 import pandas as pd
@@ -18,28 +19,75 @@ from clusterking.data.data import Data
 from clusterking.scan.scanner import Scanner
 from clusterking.cluster.cluster import Cluster
 from clusterking.benchmark.benchmark import AbstractBenchmark
+from clusterking.worker import AbstractWorker
+from clusterking.result import AbstractResult
 
 
 class NoisySampleStabilityTesterResult(StabilityTesterResult):
     """ Result of :class:`NoisySampleStabilityTester`"""
 
-    def __init__(self, df: pd.DataFrame, cached_data=None):
+    def __init__(self, df: pd.DataFrame):
         super().__init__()
         #: :class:`pd.DataFrame` containing figures of merits for each result
         self.df = df
-        self._cached_data = cached_data
 
 
-class NoisySampleStabilityTester(AbstractStabilityTester):
-    """ This stability test generates data samples with slightly varied
-    sample points (by adding :meth:`clusterking.scan.Scanner.add_spoints_noise`
-    to a pre-configured :class:`clusterking.scan.Scanner` object) and compares
-    the resulting clusters and benchmark points.
+class NoisySampleResult(AbstractResult):
+    def __init__(self, samples: Optional[List[Data]] = None):
+        super().__init__()
+        if samples is None:
+            samples = []
+        self.samples = samples
+
+    def write(
+        self, directory: Union[str, PurePath], ignore_non_empty=False
+    ) -> None:
+        """ Write to output directory
+
+        Args:
+            directory: Path to directory
+            ignore_non_empty: Ignore any files that exist in the directory.
+                These might be overwritten.
+
+        Returns:
+            None
+        """
+        directory = Path(directory)
+        if directory.exists() and not directory.is_dir():
+            raise FileExistsError(
+                "{} exists but is not a directory.".format(directory.resolve())
+            )
+        if not directory.exists():
+            directory.mkdir(parents=True)
+        if len(list(directory.iterdir())) >= 1 and not ignore_non_empty:
+            raise FileExistsError(
+                "{} is not an empty directory".format(directory.resolve())
+            )
+        for i, data in enumerate(self.samples):
+            path = directory / "data_{:04d}.sql".format(i)
+            data.write(path, overwrite="overwrite")
+
+    def load(self, directory: Union[str, PurePath]) -> None:
+        """ Load from output directory
+
+        Args:
+            directory: Path to directory to load from
+        """
+        directory = Path(directory)
+        if not directory.is_dir():
+            raise FileNotFoundError(
+                "{} does not exist or is not a directory".format(directory)
+            )
+        for path in directory.glob("data_*.sql"):
+            self.samples.append(Data(path))
 
 
-    """
-
+class NoisySample(AbstractWorker):
     def __init__(self):
+        """ This stability test generates data samples with slightly varied
+        sample points (by adding :meth:`clusterking.scan.Scanner.add_spoints_noise`
+        to a pre-configured :class:`clusterking.scan.Scanner` object)
+        """
         super().__init__()
         self._noise_kwargs = {}
         self._noise_args = []
@@ -78,17 +126,49 @@ class NoisySampleStabilityTester(AbstractStabilityTester):
         self._noise_args = args
         self._noise_kwargs = kwargs
 
-    def set_cache_data(self, value: bool) -> None:
-        """ Do we cache the :class:`~clusterking.data.Data` objects of each
-        experiment?
+    # **************************************************************************
+    # Run
+    # **************************************************************************
+
+    def run(
+        self, scanner: Scanner, data: Optional[Data] = None
+    ) -> NoisySampleResult:
+        """
 
         Args:
-            value: bool. Cache the data?
+            scanner: :class:`~clusterking.scan.scan.Scanner` object
+            data: data:  :class:`~clusterking.data.data.Data` object. This does not
+                have to contain any actual sample points, but is used so that
+                you can use data with errors by passing a
+                :class:`~clusterking.data.DataWithErrors` object.
 
         Returns:
-            None
+
         """
-        self._cache_data = value
+        scanner.set_progress_bar(False)
+        datas = []
+        for _ in tqdm.auto.tqdm(range(self._repeat + 1)):
+            noisy_scanner = copy.copy(scanner)
+            noisy_scanner.add_spoints_noise(
+                *self._noise_args, **self._noise_kwargs
+            )
+            this_data = data.copy(deep=True)
+            noisy_scanner.run(this_data).write()
+            datas.append(this_data)
+        return NoisySampleResult(datas)
+
+
+class NoisySampleStabilityTester(AbstractStabilityTester):
+    """ This stability test generates data samples with slightly varied
+    sample points (by adding :meth:`clusterking.scan.Scanner.add_spoints_noise`
+    to a pre-configured :class:`clusterking.scan.Scanner` object) and compares
+    the resulting clusters and benchmark points.
+
+
+    """
+
+    def __init__(self):
+        super().__init__()
 
     # **************************************************************************
     # Run
@@ -96,16 +176,14 @@ class NoisySampleStabilityTester(AbstractStabilityTester):
 
     def run(
         self,
-        data: Data,
-        scanner: Scanner,
-        cluster: Cluster,
+        sample: NoisySampleResult,
+        cluster: Optional[Cluster] = None,
         benchmark: Optional[AbstractBenchmark] = None,
     ) -> NoisySampleStabilityTesterResult:
         """ Run stability test.
 
         Args:
-            data: :class:`~clusterking.data.data.Data` object
-            scanner: :class:`~clusterking.scan.scan.Scanner` object
+
             cluster: :class:`~clusterking.cluster.cluster.Cluster` object
             benchmark: Optional: :class:`~clusterking.cluster.cluster.Cluster`
                 object
@@ -113,29 +191,17 @@ class NoisySampleStabilityTester(AbstractStabilityTester):
         Returns:
             :class:`~NoisySampleStabilityTesterResult` object
         """
-        scanner.set_progress_bar(False)
-        datas = []
-        original_data = None
+        reference_data = None
         fom_results = collections.defaultdict(list)
-        for _ in tqdm.auto.tqdm(range(self._repeat + 1)):
-            noisy_scanner = copy.copy(scanner)
-            if _ >= 1:
-                noisy_scanner.add_spoints_noise(
-                    *self._noise_args, **self._noise_kwargs
-                )
-            this_data = data.copy(deep=True)
-            if self._cache_data:
-                datas.append(this_data)
-            noisy_scanner.run(this_data).write()
-            cluster.run(this_data).write()
+        for isample, data in enumerate(sample.samples):
+            if cluster is not None:
+                cluster.run(data).write()
             if benchmark is not None:
-                benchmark.run(this_data).write()
-            if _ == 0:
-                original_data = this_data.copy(deep=True)
+                benchmark.run(data).write()
+            if isample == 0:
+                reference_data = data.copy(deep=True)
                 continue
             for fom_name, fom in self._foms.items():
-                fom = fom.run(original_data, this_data).fom
+                fom = fom.run(reference_data, data).fom
                 fom_results[fom_name].append(fom)
-        return NoisySampleStabilityTesterResult(
-            df=pd.DataFrame(fom_results), cached_data=datas
-        )
+        return NoisySampleStabilityTesterResult(df=pd.DataFrame(fom_results))

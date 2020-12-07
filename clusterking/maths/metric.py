3567fc65082d907e7975545d5f4757fc1934a8e5
Kilian <kilian.lieret@posteo.de>
2020-12-07 18:13:12 +0100

    [Fix] Fixing incorrect handling of correlations in chi2 metric

--- clusterking/maths/metric.py
+++ clusterking/maths/metric.py
@@ -8,6 +8,67 @@ from clusterking.maths.metric_utils import *
 from clusterking.data.dwe import DataWithErrors
 
 
+def chi2(
+    n1: np.ndarray,
+    n2: np.ndarray,
+    cov1: np.ndarray,
+    cov2: np.ndarray,
+    normalize=False,
+) -> np.ndarray:
+    """
+
+    Args:
+        n1: n_obs x n_bins
+        n2: Either n_obs x n_bins or just nbins if we're testing against a constant histogram
+        cov1: Either n_obs x n_bins x n_bins or n_bins x n_bins
+        cov2: Either n_obs x n_bins x n_bins or n_bins x n_bins
+        normalize:
+
+    Returns:
+        n_obs vector of chi2 test results (degrees of freedom not yet divided out)
+    """
+    assert n1.ndim == 2
+    n_obs, n_bins = n1.shape
+    if n2.shape == (n_obs, n_bins):
+        pass
+    elif n2.shape == (n_bins,):
+        n2 = n2.reshape((1, n_bins))
+    else:
+        raise ValueError(f"Invalid shape of n2: {n2.shape}.")
+    for _cov in [cov1, cov2]:
+        if _cov.shape == (n_obs, n_bins, n_bins):
+            pass
+        elif _cov.shape == (n_bins, n_bins):
+            pass
+        else:
+            raise ValueError(
+                f"Invalid shape of covariance matrix: {_cov.shape}"
+            )
+    if normalize:
+        if cov1.ndim == 2:
+            cov1 = np.tile(cov1, (n_obs, 1, 1))
+        if cov2.ndim == 2:
+            cov2 = np.tile(cov2, (n_obs, 1, 1))
+        norm1 = n1.sum(axis=1)
+        norm2 = n2.sum(axis=1)
+        n1 = n1.copy() / norm1.reshape((norm1.size, 1))
+        n2 = n2.copy() / norm2.reshape((norm2.size, 1))
+        cov1 = cov1.copy() / np.square(norm1).reshape((norm1.size, 1, 1))
+        cov2 = cov2.copy() / np.square(norm2).reshape((norm2.size, 1, 1))
+    diff = n1 - n2
+    cov = cov1 + cov2
+    if cov.ndim == 3:
+        return np.einsum("ni,nij,nj->n", diff, np.linalg.inv(cov), diff)
+    elif cov.ndim == 2:
+        return np.einsum("ni,ij,nj->n", diff, np.linalg.inv(cov), diff)
+    else:
+        raise ValueError(
+            "Invalid dimensionality of covariance matrix."
+            " This is likely a bug in the package. Please"
+            " report it."
+        )
+
+
 # todo: unittest
 def chi2_metric(dwe: DataWithErrors, output="condensed"):
     """
@@ -19,12 +80,9 @@ def chi2_metric(dwe: DataWithErrors, output="condensed"):
             matrix)
 
     Returns:
-        Condensed distance matrix
+        Condensed distance matrix or full distance matrix
 
     """
-    # If people try to use chi2 metric with a mere Data object, the decorrelate
-    # option doesn't exist and we'd get an ununderstandable error, so we rather
-    # cache this here.
     if not isinstance(dwe, DataWithErrors):
         raise TypeError(
             "In order to use chi2 metric, you have to use a DataWithErrors "
@@ -32,30 +90,23 @@ def chi2_metric(dwe: DataWithErrors, output="condensed"):
             "{type}. ".format(type=type(dwe))
         )
 
-    # https://root.cern.ch/doc/master/classTH1.html#a6c281eebc0c0a848e7a0d620425090a5
+    d = dwe.data()
+    n_obs, n_bins = d.shape
 
-    # n vector
-    n = dwe.norms()  # todo: this stays untouched by decorrelation, right?
-    # n x nbins
-    d = dwe.data(decorrelate=True)
-    # n x nbins
-    e = dwe.err()
-
-    # n x n x nbins
-    nom1 = np.einsum("k,li->kli", n, d)
-    nom2 = np.transpose(nom1, (1, 0, 2))
-    nominator = np.square(nom1 - nom2)
-
-    # n x n x nbins
-    den1 = np.einsum("k,li->kli", n, e)
-    den2 = np.transpose(den1, (1, 0, 2))
-    denominator = np.square(den1) + np.square(den2)
-
-    # n x n x nbins
-    summand = nominator / denominator
+    cov = dwe.cov(relative=False)
+    assert cov.shape == (n_obs, n_bins, n_bins)
 
     # n x n
-    chi2ndf = np.einsum("kli->kl", summand) / dwe.nbins
+    chi2s = np.full((n_obs, n_obs), np.nan)
+    # todo: this calculates the full n x n matrix, even though it's symmetric
+    #    so we could likely optimize this if we wanted
+    for i in range(n_obs):
+        chi2s[i, :] = chi2(d, d[i], cov, cov[i], normalize=True)
+
+    # todo: check for symmetry and vanishing diagonal of matrix here
+
+    ndf = n_bins - 1
+    chi2ndf = chi2s / np.full((1, 1), ndf)
 
     if output == "condensed":
         return condense_distance_matrix(chi2ndf)

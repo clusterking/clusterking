63a3c3f73ec2544cedda437ffd399733df99b75d
Kilian <kilian.lieret@posteo.de>
2020-12-09 11:26:41 +0100

    [Doc] Readability improvements

--- clusterking/maths/test/test_metric.py
+++ clusterking/maths/test/test_metric.py
@@ -82,11 +82,43 @@ def test_metric_symmetric(metric):
         assert np.isclose(chi2s1, chi2s2).all()
 
 
-def generate_toy_dataset(n, cov, n_toys=1000):
-    assert n.ndim == 1
-    n_bins = n.size
+def generate_toy_dataset(
+    base_hist: np.ndarray, cov: np.ndarray, n_toys=1000
+) -> np.ndarray:
+    """ Generate toy dataset around base_hist
+
+    Args:
+        base_hist: Expectation value
+        cov: Covariance matrix
+        n_toys: Number of toys to generate
+
+    Returns:
+        (n_obs, n_bins) array
+    """
+    assert base_hist.ndim == 1
+    n_bins = base_hist.size
     assert cov.shape == (n_bins, n_bins)
-    return scipy.stats.multivariate_normal.rvs(mean=n, cov=cov, size=n_toys)
+    return scipy.stats.multivariate_normal.rvs(
+        mean=base_hist, cov=cov, size=n_toys
+    )
+
+
+def _get_binned_theoretical_chi2_distribution(
+    dof: int, bins: np.ndarray
+) -> np.ndarray:
+    """
+
+    Args:
+        dof: Degrees of freedom
+        bins: Binning of chi2 distribution
+
+    Returns:
+
+    """
+    vals = scipy.stats.chi2.cdf(bins, df=dof)
+    bin_contents = vals[1:] - vals[:-1]
+    bin_contents /= bin_contents.sum()
+    return bin_contents
 
 
 @pytest.mark.slow
@@ -124,15 +156,16 @@ def test_chi2_distribution(normalize, errors):
         chi2s = chi2(
             toys, base_hist, cov, np.zeros_like(cov), normalize=normalize
         )
-        dof = n_bins - 1 if normalize else n_bins
+
         bins = np.linspace(0, 20, 30)
         ourvals, _ = np.histogram(chi2s, bins=bins,)
         ourvals = ourvals / ourvals.sum()
 
-        vals = scipy.stats.chi2.cdf(bins, df=dof)
-        bin_contents = vals[1:] - vals[:-1]
-        bin_contents /= bin_contents.sum()
+        dof = n_bins - 1 if normalize else n_bins
+        theo_expect = _get_binned_theoretical_chi2_distribution(
+            dof=dof, bins=bins
+        )
 
-        ks, pv = scipy.stats.ks_2samp(ourvals, bin_contents)
+        ks, pv = scipy.stats.ks_2samp(ourvals, theo_expect)
 
-        assert pv > 0.95, (ourvals, bin_contents)
+        assert pv > 0.95, (ourvals, theo_expect)

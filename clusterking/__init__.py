e7a3b9945034151e40bffcd2597bcebf3b3202a5
Kilian <kilian.lieret@posteo.de>
2020-12-09 11:22:08 +0100

    Urge people to upgrade their python >= 3.6

--- clusterking/__init__.py
+++ clusterking/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# std
+import sys
+
+# ours
 import clusterking.cluster
 import clusterking.data
 import clusterking.maths
@@ -8,5 +12,14 @@ import clusterking.util
 from clusterking.data import Data, DataWithErrors
 from clusterking.benchmark.benchmark import Benchmark
 from clusterking.util.metadata import get_version as _get_version
+from clusterking.util.log import get_logger
 
 version = _get_version()
+
+
+if sys.version_info < (3, 6):
+    get_logger().warning(
+        "The newer versions of ClusterKinG will require "
+        "python >= 3.6. Anything below that has reached"
+        " its end of life. Please consider upgrading."
+    )
